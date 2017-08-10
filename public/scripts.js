4208e3794c2ebf1295b9f45dc65b6f0d80e8b554
tasika16 <tasika696@gmail.com>
2017-08-10 11:52:05 +0200

    * FIX: Fixed all problems3, Right now!

--- public/scripts.js
+++ public/scripts.js
@@ -1,11 +1,10 @@
-
 var simulators = [
   { 'name': "Heavy Minigun Guy", 'identity': "T6000", 'cost': 20000 },
   { 'name': "Boba Fet", 'identity': "T5220", 'cost': 15000 }
 ];
 
 
-buildTable();
+buildTable(simulators);
 
 function sumCost() {
   var sum = 0;
@@ -15,46 +14,24 @@ function sumCost() {
   return sum;
 }
 
-function formatCurrency(simulator) {
-  return simulator.cost.toString();
-}
-
-function buildTable() {
-
-  var tr = "<tr>";
-  var trc = "</tr>";
-  var td = "<td>";
-  var tdc = "</td>";
-  var th = "<th>";
-  var thc = "</th>";
-  var delete_btn = '';
-  var table_data = "<tr>";
-  //var table_body = $('#dataTableBody')[0];
-  $('#dataTableBody').html("");
-  $('#dataTableHead').html("");
-  $('#dataTableFooter').html("");
-
-  simulators.sort(customSort);
-  //removeTable();
-
-  for (var i = 0; i < simulators.length; i++) {
-    delete_btn = '<button type="button" id="deleteBtn" class="icon-button" ><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button>';
-    table_data += td + simulators[i].name + tdc + td + simulators[i].identity + tdc + td + "$" +
-      formatCurrency(simulators[i]) + tdc + td + delete_btn + tdc + trc;
+function formatCurrency(format_cost) {
+  format_cost = format_cost.toString();
+  var counter = 0;
+  format_cost = format_cost.split("").reverse();
+  console.log(format_cost);
+  for (var i = 0; i < format_cost.length; i++) {
+    if (counter === 3) {
+      format_cost.splice(i, 0, ',');
+      counter = 0;
+    }
+    else {
+      counter++;
+    }
   }
-
-  var table_header = tr + th + "Name" + thc + th + "Type number" + thc + th + "Price" +
-    thc + th + "" + thc + trc;
-
-  var table_footer = tr + td + " " + tdc + td + simulators.length.toString() + " simulators" + tdc + td + "$" +
-    sumCost().toString() + " total" + tdc + td + tdc + trc;
-
-  //dataTableHead.innerHTML += table_header;
-  $('#dataTableHead').html(table_header);
-  //table_body.innerHTML += table_data;
-  $('#dataTableBody').html(table_data);
-  //dataTableFooter.innerHTML += table_footer;
-  $('#dataTableFooter').html(table_footer);
+  format_cost.splice(format_cost.length, 0, "$");
+  format_cost = format_cost.reverse().join("");
+  console.log(format_cost);
+  return format_cost;
 }
 
 function sumCostSearchTable(search_result) {
@@ -72,52 +49,6 @@ function filteredSimulators(query) {
   });
 }
 
-/*---SearchTable---*/
-$("#searchTxt").on("keypress", function () {
-  var tr = "<tr>";
-  var trc = "</tr>";
-  var td = "<td>";
-  var tdc = "</td>";
-  var th = "<th>";
-  var thc = "</th>";
-  var searched = $('#searchTxt').val();
-  var delete_btn = '';
-  var table_data = "<tr>";
-  $('#dataTableBody').html("");
-  $('#dataTableHead').html("");
-  $('#dataTableFooter').html("");
-
-  $('#searchErrorLabel').html("");
-  var search_result = filteredSimulators(searched);
-
-  search_result.sort(customSort);
-  //removeTable(search_result.sort(customSort));
-
-  for (var i = 0; i < search_result.length; i++) {
-    delete_btn = '<button type="button" id="deleteBtn" class="icon-button" ><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button>';
-    table_data += td + search_result[i].name + tdc + td + search_result[i].identity + tdc + td + "$" +
-      formatCurrency(search_result[i]) + tdc + td + delete_btn + tdc + trc;
-  }
-  var table_header = tr + th + "Name" + thc + th + "Type number" + thc + th + "Price" +
-    thc + th + "" + thc + trc;
-
-  var table_footer = tr + td + " " + tdc + td + search_result.length.toString() + " simulators" + tdc + td + "$" +
-    sumCostSearchTable(search_result).toString() + " total" + tdc + td + tdc + trc;
-
-  //dataTableHead.innerHTML += table_header;
-  $('#dataTableHead').html(table_header);
-  //table_body.innerHTML += table_data;
-  $('#dataTableBody').html(table_data);
-  //dataTableFooter.innerHTML += table_footer;
-  $('#dataTableFooter').html(table_footer);
-});
-/*---Reset the table after search---*/
-$('#searchTxt').on("keyup", function () {
-  if (!this.value) {
-    buildTable();
-  }
-});
-
 function customSort(a, b) {
   var name_a = a.name.toUpperCase();
   var name_b = b.name.toUpperCase();
@@ -130,8 +61,51 @@ function customSort(a, b) {
   return 0;
 }
 
+function buildTable(build_array) {
+  var tr = "<tr>";
+  var trc = "</tr>";
+  var td = "<td>";
+  var tdc = "</td>";
+  var th = "<th>";
+  var thc = "</th>";
+  var delete_btn = '';
+  var table_data = "<tr>";
+
+  build_array.sort(customSort);
+  for (var i = 0; i < build_array.length; i++) {
+    delete_btn = '<button type="button" id="deleteBtn" class="icon-button" ><i class="fa fa-trash" aria-hidden="true"></i></button>';
+    table_data += td + build_array[i].name + tdc + td + build_array[i].identity + tdc + td +
+      formatCurrency(build_array[i].cost) + tdc + td + delete_btn + tdc + trc;
+  }
+
+  var table_header = tr + th + "Name" + thc + th + "Type number" + thc + th + "Price" +
+    thc + th + "" + thc + trc;
+
+  var table_footer = tr + td + " " + tdc + td + build_array.length.toString() + " simulators" + tdc + td +
+    formatCurrency(sumCost()) + " total" + tdc + td + tdc + trc;
+
+  $('#dataTableHead').html(table_header);
+  $('#dataTableBody').html(table_data);
+  $('#dataTableFooter').html(table_footer);
+}
+
+/*---SearchTable---*/
+$("#searchTxt").on("keypress", function () {
+  var searched = $('#searchTxt').val();
+  var search_result = filteredSimulators(searched);
+  search_result.sort(customSort);
+  buildTable(search_result);
+});
+
+/*---Reset the table after search---*/
+$('#searchTxt').on("keyup", function () {
+  if (!this.value) {
+    buildTable(simulators);
+  }
+});
+
 function checkedID(tmp_identity) {
-  var is_ID = false;
+  var is_ID;
   var i = 0;
   while (i < simulators.length && simulators[i].identity !== tmp_identity.trim()) {
     i++;
@@ -155,7 +129,6 @@ function htmlEncode(str) {
 }
 
 /*---Add new row to table---*/
-
 $("#addDataToArray").on("click", function () {
 
   var tmp_name = htmlEncode($('#nameInpt').val());
@@ -178,54 +151,20 @@ $("#addDataToArray").on("click", function () {
       identity: tmp_identity,
       cost: parseInt(tmp_cost)
     };
-    //console.log(simulator);
     simulators.push(simulator);
-
-    //simulators.sort(customSort);
-    //console.log(tmp_identity);
-    //var new_row_idx = simulators.findIndex(s => s.identity === tmp_identity);
-    //buildTableInsert(new_row_idx);
-
-    buildTable();
+    buildTable(simulators);
     $("div").children().eq(2).children('input').val('');
   }
 });
-
-
-/*
-function buildTableInsert(new_row_idx){
-  var table = $('#dataTableBody')[0];
-  var new_simulator = table.insertRow(new_row_idx);
-  console.log(new_simulator.insertCell(0))
-  var new_simulator_name = new_simulator.insertCell(0);
-  var new_simulator_ID = new_simulator.insertCell(1);
-  var new_simulator_cost = new_simulator.insertCell(2);
-  var new_simulator_d_btn = new_simulator.insertCell(3);
- 
-  console.log(simulators[new_row_idx].name);
-  $("#new_simulator_name").html(simulators[new_row_idx].name);
-  $("#new_simulator_ID").html(simulators[new_row_idx].identity);
-  $("#new_simulator_cost").html(simulators[new_row_idx].cost);
-  $("#new_simulator_d_btn").html(simulators[new_row_idx].name);
- 
-  new_simulator_name = simulators[new_row_idx].name;
-  new_simulator_ID = simulators[new_row_idx].identity;
-  new_simulator_cost = simulators[new_row_idx].cost;
-  new_simulator_d_btn = simulators[new_row_idx].name;
-}*/
 
 /*---Error label set zero---*/
 $("div").children('input').on("keypress", function () {
   $(".formErLabel").html('');
 });
 
-function deleteRowFunction(btnIDX) {
-  simulators.splice(btnIDX, 1);
-  buildTable();
-}
-
+/*---Delete row from table---*/
 $('table').on("click", 'button', function () {
   console.log($(this).index(".icon-button"));
   simulators.splice(($(this).index(".icon-button")), 1);
-  buildTable();
+  buildTable(simulators);
 });
