6ef56e12df1e0d2d52ed1ebe220ae82732042d6f
tasika16 <tasika696@gmail.com>
2017-08-14 11:53:23 +0200

    * FIX: Rename the fields

--- db/create.sql
+++ db/create.sql
@@ -1,6 +1,6 @@
-CREATE TABLE simulators(
-    id UUID NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    type_number VARCHAR(255) NOT NULL,
-    price INT NOT NULL
+CREATE TABLE simulators (
+  id UUID NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  type_number VARCHAR(255) NOT NULL,
+  price INT NOT NULL
 );
--- public/scripts.js
+++ public/scripts.js
@@ -1,43 +1,43 @@
 var simulators = [
-  { 'name': "Heavy Minigun Guy", 'identity': "T6000", 'cost': 20000 },
-  { 'name': "Boba Fet", 'identity': "T5220", 'cost': 15000 }
+  { 'name': "Heavy Minigun Guy", 'type_number': "T6000", 'price': 20000 },
+  { 'name': "Boba Fet", 'type_number': "T5220", 'price': 15000 }
 ];
 
 
 buildTable(simulators);
 
-function sumCost() {
+function sumprice() {
   var sum = 0;
   for (var i = 0; i < simulators.length; i++) {
-    sum += simulators[i].cost;
+    sum += simulators[i].price;
   }
   return sum;
 }
 
-function formatCurrency(format_cost) {
-  format_cost = format_cost.toString();
+function formatCurrency(format_price) {
+  format_price = format_price.toString();
   var counter = 0;
-  format_cost = format_cost.split("").reverse();
-  console.log(format_cost);
-  for (var i = 0; i < format_cost.length; i++) {
+  format_price = format_price.split("").reverse();
+  console.log(format_price);
+  for (var i = 0; i < format_price.length; i++) {
     if (counter === 3) {
-      format_cost.splice(i, 0, ',');
+      format_price.splice(i, 0, ',');
       counter = 0;
     }
     else {
       counter++;
     }
   }
-  format_cost.splice(format_cost.length, 0, "$");
-  format_cost = format_cost.reverse().join("");
-  console.log(format_cost);
-  return format_cost;
+  format_price.splice(format_price.length, 0, "$");
+  format_price = format_price.reverse().join("");
+  console.log(format_price);
+  return format_price;
 }
 
-function sumCostSearchTable(search_result) {
+function sumpriceSearchTable(search_result) {
   var sum = 0;
   for (var i = 0; i < search_result.length; i++) {
-    sum += search_result[i].cost;
+    sum += search_result[i].price;
   }
   return sum;
 }
@@ -45,7 +45,7 @@ function sumCostSearchTable(search_result) {
 function filteredSimulators(query) {
   return simulators.filter(function (s) {
     return s.name.toLowerCase().indexOf(query.toLowerCase()) > -1
-      || s.identity.toLowerCase().indexOf(query.toLowerCase()) > -1;
+      || s.type_number.toLowerCase().indexOf(query.toLowerCase()) > -1;
   });
 }
 
@@ -74,15 +74,15 @@ function buildTable(build_array) {
   build_array.sort(customSort);
   for (var i = 0; i < build_array.length; i++) {
     delete_btn = '<button type="button" id="deleteBtn" class="icon-button" ><i class="fa fa-trash" aria-hidden="true"></i></button>';
-    table_data += td + build_array[i].name + tdc + td + build_array[i].identity + tdc + td +
-      formatCurrency(build_array[i].cost) + tdc + td + delete_btn + tdc + trc;
+    table_data += td + build_array[i].name + tdc + td + build_array[i].type_number + tdc + td +
+      formatCurrency(build_array[i].price) + tdc + td + delete_btn + tdc + trc;
   }
 
   var table_header = tr + th + "Name" + thc + th + "Type number" + thc + th + "Price" +
     thc + th + "" + thc + trc;
 
   var table_footer = tr + td + " " + tdc + td + build_array.length.toString() + " simulators" + tdc + td +
-    formatCurrency(sumCost()) + " total" + tdc + td + tdc + trc;
+    formatCurrency(sumprice()) + " total" + tdc + td + tdc + trc;
 
   $('#dataTableHead').html(table_header);
   $('#dataTableBody').html(table_data);
@@ -104,10 +104,10 @@ $('#searchTxt').on("keyup", function () {
   }
 });
 
-function checkedID(tmp_identity) {
+function checkedID(tmp_type_number) {
   var is_ID;
   var i = 0;
-  while (i < simulators.length && simulators[i].identity !== tmp_identity.trim()) {
+  while (i < simulators.length && simulators[i].type_number !== tmp_type_number.trim()) {
     i++;
   }
   if (i < simulators.length) {
@@ -132,24 +132,24 @@ function htmlEncode(str) {
 $("#addDataToArray").on("click", function () {
 
   var tmp_name = htmlEncode($('#nameInpt').val());
-  var tmp_identity = htmlEncode($('#identifyInpt').val());
-  var tmp_cost = htmlEncode($('#costInpt').val());
+  var tmp_type_number = htmlEncode($('#type_numberInpt').val());
+  var tmp_price = htmlEncode($('#priceInpt').val());
 
-  if (tmp_name.trim() === '' || tmp_identity.trim() === '' || tmp_cost.trim() === '') {
+  if (tmp_name.trim() === '' || tmp_type_number.trim() === '' || tmp_price.trim() === '') {
     $('#formErrorLabel').html("Please fill every field!");
   }
-  else if (checkedID(tmp_identity)) {
+  else if (checkedID(tmp_type_number)) {
     $('#formErrorLabel').html("This ID already exists!");
   }
-  else if (!(Number.isInteger((parseInt(tmp_cost))))) {
-    $('#formErrorLabel').html("Please fill the cost field just number!");
+  else if (!(Number.isInteger((parseInt(tmp_price))))) {
+    $('#formErrorLabel').html("Please fill the price field just number!");
   }
   else {
     $('#formErrorLabel').html("");
     var simulator = {
       name: tmp_name,
-      identity: tmp_identity,
-      cost: parseInt(tmp_cost)
+      type_number: tmp_type_number,
+      price: parseInt(tmp_price)
     };
     simulators.push(simulator);
     buildTable(simulators);
