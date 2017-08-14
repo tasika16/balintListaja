a4274cad9850754b61a11f9b2c8f27db26a6c05e
tasika16 <tasika696@gmail.com>
2017-08-14 11:21:22 +0200

    * FIX: Made new field for type_number

--- scripts/add.js
+++ scripts/add.js
@@ -1,40 +1,35 @@
 var knex = require('knex')({
-    client: 'pg',
-    connection: {
-        host: '127.0.0.1',
-        user: 'tasika161',
-        password: 'lekvarosbukta',
-        database: 'tasika161_dev'
-    }
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'tasika161',
+    password: 'lekvarosbukta',
+    database: 'tasika161_dev'
+  }
 });
 
 var bookshelf = require('bookshelf')(knex);
 
-//console.log(typeof process.argv[2]);
-//console.log(typeof process.argv[3]);
-
-var tmpName = process.argv[2].toString();
-var tmpPrice = parseInt(process.argv[3]);
-
-console.log(tmpName," : ",typeof tmpName);
-console.log(tmpPrice," : ",typeof tmpPrice);
-
 var Simulators = bookshelf.Model.extend({
-    tableName : "simulators"
+  tableName: "simulators"
 });
 
-if(process.argv.length === 4){
-    if(typeof tmpName === 'string' && typeof tmpPrice === 'number'){
-        Simulators.forge({name: tmpName, price: tmpPrice}).save()
-        .then(function (){
-            console.log("Succesful backup to database");
-            knex.destroy();
-        });
-    }
-    else{
-        console.log("Bad type error!");
-    }
+if (process.argv.length === 5) {
+  var tmp_name = process.argv[2].toString();
+  var tmp_type_number = process.argv[3].toString();
+  var tmp_price = parseInt(process.argv[4]);
+
+  if (typeof tmp_name === 'string' && typeof tmp_type_number === 'string' && typeof tmp_price === 'number') {
+    Simulators.forge({ name: tmp_name,type_number: tmp_type_number, price: tmp_price }).save()
+      .then(function () {
+        console.log("Succesful backup to database");
+        knex.destroy();
+      });
+  }
+  else {
+    console.log("Bad type error!");
+  }
 }
-else{
-    console.log("Not enough parameters");
+else {
+  console.log("Not enough parameters");
 }
--- scripts/db/create.sql
+++ scripts/db/create.sql
@@ -1,5 +1,6 @@
 CREATE TABLE simulators(
     id UUID NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    price int NOT NULL
+    type_number VARCHAR(255) NOT NULL,
+    price INT NOT NULL
 );
