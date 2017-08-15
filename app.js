94a59ddc244e3ebbc9a76a81d5a00ce862f6c43d
tasika16 <tasika696@gmail.com>
2017-08-15 16:07:50 +0200

    * NEW: Wrote the app.js end point

--- app.js
+++ app.js
@@ -1,11 +1,53 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bookshelf = require("./connection.js");
+var bodyParser = require('body-parser');
+var json_parser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+var Simulators = bookshelf.Model.extend({
+  tableName: "simulators"
+});
 
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.get('/api/simulators', function (req, res) {
+  //get the data from database
+  Simulators
+    .where(req.query)
+    .orderBy('name','ASC')
+    .fetchAll()
+    .then(function (content) {
+      res.json({ content });
+    });
+});
+
+app.post('/api/simulators', json_parser, function (req, res) {
+  //save the database
+  new Simulators({
+    'name': req.body.name,
+    'type_number': req.body.type_number,
+    'price': req.body.price
+  })
+    .save()
+    .then(function (content) {
+      res.json({ content });
+    })
+});
+
+app.delete('/api/simulators/:id', function (req, res) {
+  //delete from the database
+  Simulators
+    .where('id', req.params.id)
+    .destroy()
+    .then(function (content) {
+      res.send({ content });
+    });
 });
 
 app.listen(3000);
